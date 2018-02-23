e2243d052a5c7bd4c61096bf093da705209b74a8
Arun Ponnusamy <arunponnusamy@hotmail.com>
2018-02-23 18:24:00 +0530

    Add setup files

---
+++ cvlib/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='cvlib',
+      version='0.1.0',
+      description='open source high level computer vision library',
+      url='https://github.com/arunponnusamy/cvlib.git',
+      author='Arun Ponnusamy',
+      author_email='arunponnusamy93@gmail.com',
+      license='MIT',
+      packages=['cvlib'],
+      zip_safe=False)
