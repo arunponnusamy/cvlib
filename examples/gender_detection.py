89ffe8c2b68d60c72ab84a404b675f0b7dcf961d
Arun Ponnusamy <arunponnusamy@hotmail.com>
2018-12-02 14:09:15 +0530

    Add gender detection

---
+++ examples/gender_detection.py
@@ -0,0 +1,55 @@
+# author: Arun Ponnusamy
+# website: https://www.arunponnusamy.com
+
+# gender detection example
+# usage: python gender_detection.py <input_image>
+
+# import necessary packages
+import cv2
+import cvlib as cv
+import sys
+import numpy as np
+
+# read input image
+img = cv2.imread(sys.argv[1])
+
+# apply face detection
+face, conf = cv.detect_face(img)
+
+# loop through detected faces
+for f in face:
+
+    (startX,startY) = f[0],f[1]
+    (endX,endY) = f[2],f[3]
+    
+    # draw rectangle over face
+    cv2.rectangle(img, (startX,startY), (endX,endY), (0,255,0), 2)
+
+    face_crop = np.copy(img[startY:endY, startX:endX])
+
+    # apply gender detection
+    (label, confidence) = cv.detect_gender(face_crop)
+
+    print(confidence)
+    print(label)
+
+    idx = np.argmax(confidence)
+    label = label[idx]
+
+    label = "{}: {:.2f}%".format(label, confidence[idx] * 100)
+
+    Y = startY - 10 if startY - 10 > 10 else startY + 10
+
+    cv2.putText(img, label, (startX, Y),  cv2.FONT_HERSHEY_SIMPLEX,
+                0.7, (0, 255, 0), 2)
+
+# display output
+# press any key to close window           
+cv2.imshow("gender detection", img)
+cv2.waitKey()
+
+# save output
+cv2.imwrite("gender_detection.jpg", img)
+
+# release resources
+cv2.destroyAllWindows()
---
+++ examples/gender_detection_video.py
@@ -0,0 +1,74 @@
+# author: Arun Ponnusamy
+# website: https://www.arunponnusamy.com
+
+# gender detection video example
+# usage: python gender_detection_video.py <video_file.mp4>
+
+# import necessary packages
+import cvlib as cv
+import cv2
+import numpy as np
+import sys
+
+# open video file
+webcam = cv2.VideoCapture(sys.argv[1])
+
+if not webcam.isOpened():
+    print("Could not open video file")
+    exit()
+    
+
+# loop through frames
+while webcam.isOpened():
+
+    # read frame from video 
+    status, frame = webcam.read()
+
+    if not status:
+        print("Could not read frame")
+        exit()
+
+    # apply face detection
+    face, confidence = cv.detect_face(frame)
+
+    print(face)
+    print(confidence)
+
+    # loop through detected faces
+    for idx, f in enumerate(face):
+        
+        (startX, startY) = f[0], f[1]
+        (endX, endY) = f[2], f[3]
+
+        # draw rectangle over face
+        cv2.rectangle(frame, (startX,startY), (endX,endY), (0,255,0), 2)
+
+        face_crop = np.copy(frame[startY:endY, startX:endX])
+
+        # apply face detection    
+        (label, confidence) = cv.detect_gender(face_crop)
+
+        print(confidence)
+        print(label)
+
+        idx = np.argmax(confidence)
+        label = label[idx]
+
+        label = "{}: {:.2f}%".format(label, confidence[idx] * 100)
+
+        Y = startY - 10 if startY - 10 > 10 else startY + 10
+
+        # write detected gender and confidence percentage on top of face rectangle
+        cv2.putText(frame, label, (startX,Y), cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+                    (0,255,0), 2)
+
+    # display output
+    cv2.imshow("Real-time gender detection", frame)
+
+    # press "Q" to stop
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+# release resources
+webcam.release()
+cv2.destroyAllWindows()        
