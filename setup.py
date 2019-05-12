6ee5b8efbcb863f6f78146d160c2fdb31a98489f
Arun Ponnusamy <arunponnusamy@hotmail.com>
2019-05-12 18:27:41 +0530

    Add install_requires and long description

--- setup.py
+++ setup.py
@@ -1,17 +1,9 @@
-import os
-
 from setuptools import setup
 
-# allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-
-with open('requirements.txt') as f:
-      install_requires = f.read().splitlines()
-
-setup(
-      name='cvlib',
-      version='0.2.0',
+setup(name='cvlib',
+      version='0.2.1',
       description='A high level, easy to use, open source computer vision library for python',
+      long_description='A high level, easy to use, open source computer vision library for python',        
       url='https://github.com/arunponnusamy/cvlib.git',
       author='Arun Ponnusamy',
       author_email='hello@arunponnusamy.com',
@@ -19,8 +11,5 @@ setup(
       packages=['cvlib'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=install_requires,
-      extras_require={
-            'gpu':  ['tensorflow-gpu'],
-      },
-)
+      install_requires=['numpy', 'progressbar', 'requests', 'pillow', 'keras']
+      )
