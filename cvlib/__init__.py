19699c5bf6ab18213e21721f1279bf9e88668154
Arun Ponnusamy <arunponnusamy@hotmail.com>
2019-05-12 18:31:21 +0530

    Update version to 0.2.1

--- cvlib/__init__.py
+++ cvlib/__init__.py
@@ -1,7 +1,7 @@
 # author: Arun Ponnusamy
 # website: https://www.arunponnusamy.com
 
-__version__ = "0.1.7"
+__version__ = "0.2.1"
 
 from .face_detection import detect_face
 from .object_detection import detect_common_objects
