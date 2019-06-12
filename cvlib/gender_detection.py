85eaf33af688d05a4c62545c864599a9ec6edb32
Sutrisno <kangfend@gmail.com>
2019-06-12 09:11:26 +0700

    Create cache_dir if not exists

--- cvlib/gender_detection.py
+++ cvlib/gender_detection.py
@@ -8,41 +8,38 @@ from keras.preprocessing.image import img_to_array
 is_initialized = False
 model = None
 
+
 def pre_process(face):
 
-    face = cv2.resize(face, (96,96))
+    face = cv2.resize(face, (96, 96))
     face = face.astype("float") / 255.0
     face = img_to_array(face)
     face = np.expand_dims(face, axis=0)
 
     return face
 
-def detect_gender(face):
 
+def detect_gender(face):
     global is_initialized
     global model
 
     labels = ['man', 'woman']
 
-    
     if not is_initialized:
-
         print("[INFO] initializing ... ")
-        
         dwnld_link = "https://github.com/arunponnusamy/cvlib/releases/download/v0.2.0/gender_detection.model"
+        cache_dir = os.path.join(os.path.expanduser('~'), '.cvlib', 'pre-trained')
+
+        if not os.path.isdir(cache_dir):
+            os.makedirs(cache_dir)
 
         model_path = get_file("gender_detection.model", dwnld_link,
-                              cache_dir= os.path.expanduser('~') + os.path.sep + '.cvlib' + os.path.sep + 'pre-trained') 
-
+                              cache_dir=cache_dir)
         model = load_model(model_path)
-
         is_initialized = True
 
-
     face = pre_process(face)
-
     conf = model.predict(face)[0]
-
     return (labels, conf)
 
-        
+
